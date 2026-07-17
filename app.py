8ad39d283069ec22d3ef9fee62704cf53af7cc4a
sanskritinarain <sanskritinarain2005@gmail.com>
2026-07-17 12:34:55 +0530

    2nd api getAllUploadedDocuments created
    Added SQLite database integration to store document metadata.

--- app.py
+++ app.py
@@ -1,16 +1,30 @@
 from fastapi import FastAPI, UploadFile, File
 import os
 from task_1 import main as process_document
+import sqlite3
 
 app = FastAPI()
 
 UPLOAD_DIR = "uploaded_docs"
 os.makedirs(UPLOAD_DIR, exist_ok=True)
 
+# Initialize db
+conn = sqlite3.connect("chunks.db")
+cursor = conn.cursor()
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS documents (
+        doc_id TEXT PRIMARY KEY,
+        doc_name TEXT
+    )
+""")
+conn.commit()
+conn.close()
+
 @app.get("/")
 def health_check():
     return {"status": "API is running"}
 
+# FIRST API (NEW DOC UPLOAD
 @app.post("/uploadDocument")
 async def upload_document(file: UploadFile = File(...)):
     contents = await file.read()
@@ -29,3 +43,17 @@ async def upload_document(file: UploadFile = File(...)):
         "filename": file.filename,
         "message": "Document uploaded and processed successfully"
     }
+
+
+# SECOND API (FETCH ALL UPLOADED DOCS INFO)
+@app.get("/getAllUploadedDocuments")
+def get_all_uploaded_documents():
+    conn = sqlite3.connect("chunks.db")
+    cursor = conn.cursor()
+    cursor.execute("SELECT doc_id, doc_name FROM documents")
+    rows = cursor.fetchall()
+    conn.close()
+
+    documents = [{"doc_id": row[0], "doc_name": row[1]} for row in rows]
+
+    return {"documents": documents}
